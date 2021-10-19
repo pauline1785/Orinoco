e28b8ae0f0f2e82e78ed1f4cd0ad140a3d796afe
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-10-19 12:09:32 +0200

    create class form

---
+++ frontend/models/form.js
@@ -0,0 +1,10 @@
+class Form{
+    constructor(){
+        this.name = document.querySelector("#lastName").value;
+        this.firstName = document.querySelector("#firstName").value;
+        this.email = document.querySelector("#email").value;
+        this.address = document.querySelector("#address").value;
+        this.postalCode = document.querySelector("#postalCode").value;
+        this.city = document.querySelector("#city").value;
+    }
+}
