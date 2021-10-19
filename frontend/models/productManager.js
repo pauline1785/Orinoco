523061db3c929396ed9fc9f62c28a169dcfcb10d
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-10-19 12:09:43 +0200

    update

--- frontend/models/productManager.js
+++ frontend/models/productManager.js
@@ -1,31 +1,3 @@
-/*function addProduct(productId){
-    let productAdd = getProduct();
-    productAdd.push({id: productId, Image: product.imageUrl, Name: product.name, Price : product.price});
-    saveProduct(productAdd);
-}
-
-function removeProduct(productId){
-    let productAdd = getProduct();
-    productAdd = productAdd.filter(product => product._id != productId);
-    saveProduct(productAdd);
-}
-
-function getProduct(){
-    let productAdd = localStorage.getItem("product");
-    if(productAdd == null){
-        return [];
-    }else{
-        return JSON.parse(productAdd);
-    }
-}
-
-function getProductId(){
-    return getProduct().map(product => product._id);
-}
-
-function saveProduct(productAdd){
-    localStorage.setItem("productAdd",JSON.stringify(productAdd));
-}*/
 
 
 
