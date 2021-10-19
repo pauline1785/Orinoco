bdedbf3974fba0be5d923e871c848e42a2a4289a
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-10-19 12:10:59 +0200

    add validation form and send server

--- frontend/view/cart/cart.js
+++ frontend/view/cart/cart.js
@@ -1,6 +1,6 @@
-/* AFFICHAGE DES DONNES RECUPEREES DANS LE LOCALSTORAGE */
+/* AFFICHAGE DES PRODUITS DU LOCALSTORAGE + PRIX TOTAL + ENVOI DE LA COMMANDE */
 
-// récupération du local Storage
+// récupération du local storage
 let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
 
 /* **** Affichage des produits dans le panier **** */
@@ -13,55 +13,52 @@ let structureProductCart = [];
 if(productInLocalStorage === null || productInLocalStorage == 0){
     const emptyCart = ` 
     <section class="cart__items">
-        <div>Le panier est vide</div>
+        <p>Le panier est vide</p>
     </section> 
     `;
     cartHtml.innerHTML = emptyCart;
 }else{
-    for(k=0; k < productInLocalStorage.length; k++){
+    for(i=0; i < productInLocalStorage.length; i++){
         structureProductCart = structureProductCart + `
         <section class="cart__items">
             <div class="cart__items__img">
-                <img src="${productInLocalStorage[k].imageUrl}" alt="Photo d'un ours en peluche">
+                <img src="${productInLocalStorage[i].imageUrl}" alt="Photo d'un ours en peluche">
             </div>
             <div class="cart__items__content">
-                <h2>${productInLocalStorage[k].name}</h2>
-                <p>${productInLocalStorage[k].price}€</p>
+                <h2>${productInLocalStorage[i].name}</h2>
+                <p>${productInLocalStorage[i].price}€</p>
                 <p>Quantité</p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="1">
-                <button class="item_delete"><i class="far fa-trash-alt"></i></button>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="10" value="1">
+                <button class="item_delete" data-parent="${productInLocalStorage[i].id}"><i class="far fa-trash-alt"></i></button>
             </div>
         </section>
          `;
     }
-    if(k == productInLocalStorage.length){
+    if(i == productInLocalStorage.length){
         cartHtml.innerHTML = structureProductCart;
     }
 };
 
 // bouton supprimer un article du panier
-/*let productDelete = document.querySelector(".item_delete");
+let productDeleteTab = document.querySelector(".item_delete");
 
-for(let l = 0; l < productDelete.length; l++){
-    productDelete.addEventListener("click", function(event) {
+    productDeleteTab.addEventListener("click", function(event){
         event.preventDefault();
+        let btnDelete = event.target;
 
-        let idSelectDelete = productInLocalStorage[l]._id; 
-
+        let idSelectDelete = productInLocalStorage[0].id; 
         productInLocalStorage = productInLocalStorage.filter(elt => elt.id !== idSelectDelete);
+        localStorage.setItem("product", JSON.stringify(productInLocalStorage));  
+});
 
-        localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-        
-    })
-}*/
 
 
 /* **** Total du panier **** */
 let calculTotal = [];
 
 // aller chercher tous les prix
-for(let m = 0; m < productInLocalStorage.length; m ++){
-    let productPriceInCart = productInLocalStorage[m].price;
+for(let i = 0; i < productInLocalStorage.length; i ++){
+    let productPriceInCart = productInLocalStorage[i].price;
 
     calculTotal.push(productPriceInCart)
 }
@@ -83,43 +80,44 @@ cartHtml.insertAdjacentHTML("beforeend", totalPriceHTML);
 /* **** Formulaire validation commande **** */
 const orderHTML = () => {
     const formHTML = document.querySelector(".cart");
-const formStructure = `
-    <section class="cart__order">
-        <h3>Finaliser la commande :</h3>
-        <form method="get" class="cart__order__form">
-            <div class="cart__order__form__question">
-                <label for="lastName">Nom: </label>
-                <input type="text" name="lastName" id="lastName" required>
-            </div>
-            <div class="cart__order__form__question">
-                <label for="firstName">Prénom: </label>
-                <input type="text" name="firstName" id="firstName" required>
-            </div>
-            <div class="cart__order__form__question">
-                <label for="email">Email: </label>
-                <input type="email" name="email" id="email" required>
-            </div>
-            <div class="cart__order__form__question">
-                <label for="address">Adresse: </label>
-                <input type="text" name="address" id="address" required>
-            </div>
-            <div class="cart__order__form__question">
-                <label for="postalCode">Code postal: </label>
-                <input type="text" name="postalCode" id="postalCode" required>
-            </div>
-            <div class="cart__order__form__question">
-                <label for="city">Ville: </label>
-                <input type="text" name="city" id="city" required>
-            </div>
-            <div class="cart__order__form__submit">
-                <button type="submit" id="order">Valider la commande</button>
-            </div>
-        </form>
-    </section>
-`;
+    const formStructure = `
+        <section class="cart__order">
+            <h3>Finaliser la commande :</h3>
+            <form method="get" class="cart__order__form">
+                <div class="cart__order__form__question">
+                    <label for="lastName">Nom: </label>
+                    <input type="text" name="lastName" id="lastName" required>
+                </div>
+                <div class="cart__order__form__question">
+                    <label for="firstName">Prénom: </label>
+                    <input type="text" name="firstName" id="firstName" required>
+                </div>
+                <div class="cart__order__form__question">
+                    <label for="email">Email: </label>
+                    <input type="email" name="email" id="email" required>
+                    <br><span id="email_invalid"></span>
+                </div>
+                <div class="cart__order__form__question">
+                    <label for="address">Adresse: </label>
+                    <input type="text" name="address" id="address" required>
+                </div>
+                <div class="cart__order__form__question">
+                    <label for="postalCode">Code postal: </label>
+                    <input type="text" name="postalCode" id="postalCode" required>
+                    <br><span id="postalcode_invalid"></span>
+                </div>
+                <div class="cart__order__form__question">
+                    <label for="city">Ville: </label>
+                    <input type="text" name="city" id="city" required>
+                </div>
+                <div class="cart__order__form__submit">
+                    <button type="submit" id="order">Valider la commande</button>
+                </div>
+            </form>
+        </section>
+    `;
 
-formHTML.insertAdjacentHTML("beforeend", formStructure);
-
+    formHTML.insertAdjacentHTML("beforeend", formStructure);
 };
 
 orderHTML();
@@ -127,32 +125,64 @@ orderHTML();
 
 /* **** Récupération des données du formulaire **** */
 //sélection du bouton submit pour faire un event listener dessus
-const buttonSubmit = document.querySelector("#order");
+const buttonSubmitForm = document.querySelector("#order");
 
-buttonSubmit.addEventListener("click", function(event){
+buttonSubmitForm.addEventListener("click", function(event){
     event.preventDefault();
     //récupération des données du formulaire
-    let formOptions = {
-        name: document.querySelector("#lastName").value,
-        firstName : document.querySelector("#firstName").value,
-        email : document.querySelector("#email").value,
-        address : document.querySelector("#address").value,
-        postalCode : document.querySelector("#postalCode").value,
-        city : document.querySelector("#city").value,
+    const formValues = new Form();
+
+    //validation du formulaire en utilisant les Expressions Régulières (regex)
+    const regExEmail = (value) =>{
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
     }
 
-    //validation du formulaire
+    const regExCodePostal = (value) =>{
+        return /^[0-9]{5}$/.test(value);
+    }
 
+    //controle de la validité de l'email
+    function emailControl(){
+        const emailValid = formValues.email;
+        if(regExEmail(emailValid)){
+            return true;
+        }else{
+            document.querySelector("#email_invalid").textContent = "Veuillez saisir un email valide.";
+            return false;
+        }
+    }
+    //controle de la validité du code postal
+    function postalCodeControl(){
+        const postalCodeValid = formValues.postalCode;
+        if(regExCodePostal(postalCodeValid)){
+            return true;
+        }else{
+            document.querySelector("#postalcode_invalid").textContent = "Veuillez saisir un Code Postal à 5 chiffres.";
+            return false;
+        }
+    }
 
-    // mettre les données du formulaire dans le local storage
-    localStorage.setItem("formOptions", JSON.stringify(formOptions));
+    // mettre les données du formulaire dans le local storage si le formulaire est validé
+    if(emailControl() && postalCodeControl()){
+        localStorage.setItem("formValues", JSON.stringify(formValues));
+    }else{
+        alert("Veuillez remplir le formulaire.");
+    }
     
     //regroupement des produits et des données du formulaire
     const submitOrder  = {
-        productInLocalStorage, formOptions
+        productInLocalStorage, formValues
     }
     console.log("submitOrder");
     console.log(submitOrder);
 
+    //envoi de la commande vers le server
+    const response = fetch('http://localhost:3000/api/teddies/order', {
+        method: 'POST',
+        body: JSON.stringify(submitOrder),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
 });
 
