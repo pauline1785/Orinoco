9c02cd5d485b276ddb5aca991fdf8395483953fe
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-10-20 16:29:31 +0200

    change name by lastname

--- frontend/models/form.js
+++ frontend/models/form.js
@@ -1,6 +1,6 @@
 class Form{
     constructor(){
-        this.name = document.querySelector("#lastName").value;
+        this.lastname = document.querySelector("#lastName").value;
         this.firstName = document.querySelector("#firstName").value;
         this.email = document.querySelector("#email").value;
         this.address = document.querySelector("#address").value;
