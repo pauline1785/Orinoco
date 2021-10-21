ed58db7099367f32159e8a3f30b94a4bb398a168
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-10-21 18:30:35 +0200

    delete postalcode

--- frontend/models/form.js
+++ frontend/models/form.js
@@ -4,7 +4,6 @@ class Form{
         this.firstName = document.querySelector("#firstName").value;
         this.email = document.querySelector("#email").value;
         this.address = document.querySelector("#address").value;
-        this.postalCode = document.querySelector("#postalCode").value;
         this.city = document.querySelector("#city").value;
     }
 }
