0407fa088b22e6ecd5a9059c21ad309ded846eb8
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2021-10-28 18:26:38 +0200

    update comment

--- frontend/view/home.js
+++ frontend/view/home.js
@@ -1,7 +1,7 @@
 /* APPEL DE L'API ET AFFICHAGE DE TOUS LES PRODUITS */
 
 fetch("http://localhost:3000/api/teddies")
-    //récupère la réponse du fetch
+    // récupère la réponse du fetch
     .then(function(response){ 
         if(response.ok ){
             let teddies = response.json();
